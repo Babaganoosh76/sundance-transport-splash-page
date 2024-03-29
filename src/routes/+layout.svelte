a308a53a9d1553ec602613cdd6792c42c9c6273d
amiller5233@gmail.com <amiller5233@gmail.com>
2024-03-28 22:54:03 -0700

    install svelte and start designing homepage

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,69 @@
+<script>
+	import "../scss/style.scss";
+	const currentYear = (new Date().getFullYear());
+</script>
+
+<nav class="navbar bg-dark border-bottom border-body navbar-expand-lg py-0" data-bs-theme="dark">
+	<div class="container">
+		<a class="navbar-brand py-0" href="#">
+			<img src="sunburst-half.png" class="mt-3" alt="Sundance Transport Logo" width="120" height="60">
+		</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav flex-grow-1 justify-content-center mb-2 mb-lg-0">
+				<li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="#">Home</a>
+				</li>
+				
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+						Services
+					</a>
+					<ul class="dropdown-menu">
+						<li><a class="dropdown-item" href="#">Action</a></li>
+						<li><a class="dropdown-item" href="#">Another action</a></li>
+						<li><hr class="dropdown-divider"></li>
+						<li><a class="dropdown-item" href="#">Something else here</a></li>
+					</ul>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">About Us</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Contact</a>
+				</li>
+			</ul>
+			<button class="btn btn-primary fw-bold px-4">(916) 933-2870</button>
+		</div>
+	</div>
+</nav>
+
+<slot></slot>
+
+<footer class="bg-dark text-body" data-bs-theme="dark">
+	<div class="container py-5">
+		<div class="row align-items-center">
+			<div class="col-md-4">
+				<img src="sunburst-half.png" width="60" height="30" class="mb-1">
+				<p class="h4 mb-1">Sundance Transport, Inc.</p>
+				<p class="small text-body-secondary mb-1">Copyright &copy; { currentYear }</p>
+				<p class="small text-body-secondary mb-1">Managed by Adam Miller</p>
+			</div>
+			<div class="col-md-4">
+				<p class="h5">Contact</p>
+				<p class="mb-1"><a href="tel:9169332870">(916) 933-2870</a>
+				<p class="mb-1"><a href="mailto:sundancetransportinc@gmail.com">sundancetransportinc@gmail.com</a></p>
+			</div>
+			<div class="col-md-4">
+				<p class="h5">Follow us!</p>
+				<div class="d-flex">
+					<button class="btn btn-link">F</button>
+					<button class="btn btn-link">L</button>
+					<button class="btn btn-link">Y</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,104 @@
+<svelte:head>
+	<title>Sundance Transport, Inc | Flatbed, Hotshot & Forklift Services</title>
+	<meta name="description" content="Looking for reliable trucking solutions in Sacramento, CA? Sundance is a family-owned business that offers flatbed, hotshot, and forklift services tailored to your needs. Trust our experienced team for efficient logistics and personalized customer care. Get a quote today!." />
+</svelte:head>
+
+<section class="position-relative">
+	<div id="carouselHero" class="carousel slide" style="height: 50vh;" data-bs-ride="carousel" data-bs-keyboard="false">
+		<div class="carousel-inner h-100">
+			<div class="carousel-item active h-100">
+				<img src="hero1.jpg" class="w-100 h-100 object-fit-cover" style="object-position: center;" alt="Three flatbed Sundance trucks parked in a row">
+			</div>
+			<div class="carousel-item h-100">
+				<img src="hero2.jpg" class="w-100 h-100 object-fit-cover" style="object-position: center;" alt="A hotshot truck with a loaded gooseneck trailer">
+			</div>
+			<div class="carousel-item h-100">
+				<img src="hero3.jpg" class="w-100 h-100 object-fit-cover" style="object-position: center;" alt="Three flatbed Sundance trucks parked in a yard">
+			</div>
+		</div>
+		<div class="position-absolute top-0 left-0 w-100 h-100 d-flex align-items-center" style="background-color:rgba(0, 0, 0, 0.5);">
+			<div class="container text-center text-light py-section">
+				<div>
+					<h1 class="display-2 fw-bold">Sundance Transport, Inc.</h1>
+					<p class="fst-italic">&mdash; Your Trusted Partner for Flatbed, Hotshot, and Forklift Services &mdash;</p>
+					<button class="btn btn-primary btn-lg h6 px-4">Get a Quote</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="accent-horizontal"></div>
+
+<section>
+	<div class="container py-section">
+		<div class="d-flex align-items-center mb-4">
+			<h2 class="flex-shrink-0">Our services</h2>
+			<hr class="flex-shrink-1 ms-4 w-100">
+		</div>
+		<div class="row g-5">
+			<div class="col-md-4">
+				<div class="card h-100">
+					<img src="service-trucking.jpg" class="card-img-top" alt="...">
+				  <div class="card-body position-relative">
+				  	<p class="h4">Flatbed Trucking</p>
+				    <p class="card-text">Our flatbed trucking service offers unparalleled versatility and reliability for transporting oversized or irregularly shaped cargo. With a fleet equipped to handle diverse loads, we ensure secure and efficient transportation for your goods, whether it's construction materials, machinery, or equipment.</p>
+				    <div class="position-absolute bottom-0 end-0 ratio ratio-1x1 overflow-hidden" style="width: 65%;">
+				    	<img src="sunburst-half.png" class="w-auto" style="opacity:0.2; filter:grayscale(80%);">
+				    </div>
+				  </div>
+				  <div class="card-footer">
+				  	<button class="btn btn-link w-100 text-decoration-none">
+				  		<span class="align-middle">Learn more</span>
+				  		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="align-middle lh-1 ms-2" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>
+				  	</button>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card h-100">
+					<img src="service-hotshot.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				  	<p class="h4">Hotshot Trucking</p>
+				    <p class="card-text">Need expedited delivery? Our hotshot trucking service provides swift and reliable transportation solutions for time-sensitive shipments. With a dedicated team and flexible scheduling, we prioritize urgent deliveries without compromising safety or quality, ensuring your goods reach their destination on time, every time.</p>
+				  </div>
+				  <div class="card-footer">
+				  	<button class="btn btn-link w-100 text-decoration-none">
+				  		<span class="align-middle">Learn more</span>
+				  		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="align-middle lh-1 ms-2" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>
+				  	</button>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card h-100">
+					<img src="service-forklift.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				  	<p class="h4">Forklift Trucking</p>
+				    <p class="card-text">Our forklift capabilities extend beyond transportation to streamline your logistics operations. From loading and unloading to material handling, our skilled operators and well-maintained equipment ensure seamless transitions throughout the supply chain. Trust us to optimize efficiency and productivity while prioritizing the safety of your cargo.</p>
+				  </div>
+				  <div class="card-footer">
+				  	<button class="btn btn-link w-100 text-decoration-none">
+				  		<span class="align-middle">Learn more</span>
+				  		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="align-middle lh-1 ms-2" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>
+				  	</button>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container py-section">
+		<div class="row g-5 align-items-center">
+			<div class="col-md-6">
+				<img src="why1.jpg" class="img-fluid rounded-4 shadow-sm" alt="Hotshot truck with assorted materials loaded on the trailer">
+			</div>
+			<div class="col-md-6">
+				<h2>Why Choose <span class="text-primary">Sundance</span>?</h2>
+				<p>When you pick our family-owned trucking crew, you're not just choosing a service – you're joining our family. We're all about flexibility, making sure we adapt to what works best for you. And since we know Sacramento like the back of our hand, you can bet we'll find the quickest routes and dodge any local hiccups along the way. Count on us for a laid-back yet reliable ride for your stuff!</p>
+			</div>
+		</div>
+	</div>
+</section>
