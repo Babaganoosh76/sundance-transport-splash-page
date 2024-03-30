788d6a2b247023d43efa2645c01d21ad68ce01f1
amiller5233@gmail.com <amiller5233@gmail.com>
2024-03-29 22:25:59 -0700

    Finish/polish homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,56 +33,62 @@
 <section>
 	<div class="container py-section">
 		<div class="d-flex align-items-center mb-4">
-			<h2 class="flex-shrink-0">Our services</h2>
+			<h2 class="flex-shrink-0">Our Services</h2>
 			<hr class="flex-shrink-1 ms-4 w-100">
 		</div>
-		<div class="row g-5">
+		<div class="row gx-md-5 gy-5">
 			<div class="col-md-4">
 				<div class="card h-100">
 					<img src="service-trucking.jpg" class="card-img-top" alt="...">
-				  <div class="card-body position-relative">
-				  	<p class="h4">Flatbed Trucking</p>
-				    <p class="card-text">Our flatbed trucking service offers unparalleled versatility and reliability for transporting oversized or irregularly shaped cargo. With a fleet equipped to handle diverse loads, we ensure secure and efficient transportation for your goods, whether it's construction materials, machinery, or equipment.</p>
-				    <div class="position-absolute bottom-0 end-0 ratio ratio-1x1 overflow-hidden" style="width: 65%;">
-				    	<img src="sunburst-half.png" class="w-auto" style="opacity:0.2; filter:grayscale(80%);">
-				    </div>
-				  </div>
-				  <div class="card-footer">
-				  	<button class="btn btn-link w-100 text-decoration-none">
-				  		<span class="align-middle">Learn more</span>
-				  		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="align-middle lh-1 ms-2" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>
-				  	</button>
-				  </div>
+					<div class="card-body" style="z-index:1;">
+						<p class="h4">Flatbed Trucking</p>
+						<p class="card-text">Our flatbed trucking service offers unparalleled versatility and reliability for transporting oversized or irregularly shaped cargo. With a fleet equipped to handle diverse loads, we ensure secure and efficient transportation for your goods, whether it's construction materials, machinery, or equipment.</p>
+					</div>
+					<div class="card-footer bg-transparent border-0 pt-0 pb-4" style="z-index:1;">
+						<button class="btn btn-link text-start w-100 text-decoration-none">
+							<span class="align-middle">Learn more</span>
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="align-middle lh-1 ms-2" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>
+						</button>
+					</div>
+					<div class="position-absolute bottom-0 end-0 ratio ratio-1x1 overflow-hidden" style="width: 65%;">
+						<img src="sunburst-half.png" class="w-auto" style="opacity:0.2; filter:grayscale(80%);" draggable="false">
+					</div>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="card h-100">
 					<img src="service-hotshot.jpg" class="card-img-top" alt="...">
-				  <div class="card-body">
-				  	<p class="h4">Hotshot Trucking</p>
-				    <p class="card-text">Need expedited delivery? Our hotshot trucking service provides swift and reliable transportation solutions for time-sensitive shipments. With a dedicated team and flexible scheduling, we prioritize urgent deliveries without compromising safety or quality, ensuring your goods reach their destination on time, every time.</p>
-				  </div>
-				  <div class="card-footer">
-				  	<button class="btn btn-link w-100 text-decoration-none">
-				  		<span class="align-middle">Learn more</span>
-				  		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="align-middle lh-1 ms-2" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>
-				  	</button>
-				  </div>
+					<div class="card-body" style="z-index:1;">
+						<p class="h4">Hotshot Trucking</p>
+						<p class="card-text">Need expedited delivery? Our hotshot trucking service provides swift and reliable transportation solutions for time-sensitive shipments. With a dedicated team and flexible scheduling, we prioritize urgent deliveries without compromising safety or quality, ensuring your goods reach their destination on time, every time.</p>
+					</div>
+					<div class="card-footer bg-transparent border-0 pt-0 pb-4" style="z-index:1;">
+						<button class="btn btn-link text-start w-100 text-decoration-none">
+							<span class="align-middle">Learn more</span>
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="align-middle lh-1 ms-2" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>
+						</button>
+					</div>
+					<div class="position-absolute bottom-0 end-0 ratio ratio-1x1 overflow-hidden" style="width: 65%;">
+						<img src="sunburst-half.png" class="w-auto" style="opacity:0.2; filter:grayscale(80%);" draggable="false">
+					</div>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="card h-100">
 					<img src="service-forklift.jpg" class="card-img-top" alt="...">
-				  <div class="card-body">
-				  	<p class="h4">Forklift Trucking</p>
-				    <p class="card-text">Our forklift capabilities extend beyond transportation to streamline your logistics operations. From loading and unloading to material handling, our skilled operators and well-maintained equipment ensure seamless transitions throughout the supply chain. Trust us to optimize efficiency and productivity while prioritizing the safety of your cargo.</p>
-				  </div>
-				  <div class="card-footer">
-				  	<button class="btn btn-link w-100 text-decoration-none">
-				  		<span class="align-middle">Learn more</span>
-				  		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="align-middle lh-1 ms-2" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>
-				  	</button>
-				  </div>
+					<div class="card-body" style="z-index:1;">
+						<p class="h4">Forklifts</p>
+						<p class="card-text">Our forklift capabilities extend beyond transportation to streamline your logistics operations. From loading and unloading to material handling, our skilled operators and well-maintained equipment ensure seamless transitions throughout the supply chain. Trust us to optimize efficiency and productivity while prioritizing the safety of your cargo.</p>
+					</div>
+					<div class="card-footer bg-transparent border-0 pt-0 pb-4" style="z-index:1;">
+						<button class="btn btn-link text-start w-100 text-decoration-none">
+							<span class="align-middle">Learn more</span>
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="align-middle lh-1 ms-2" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>
+						</button>
+					</div>
+					<div class="position-absolute bottom-0 end-0 ratio ratio-1x1 overflow-hidden" style="width: 65%;">
+						<img src="sunburst-half.png" class="w-auto" style="opacity:0.2; filter:grayscale(80%);" draggable="false">
+					</div>
 				</div>
 			</div>
 		</div>
@@ -91,13 +97,73 @@
 
 <section>
 	<div class="container py-section">
-		<div class="row g-5 align-items-center">
+		<div class="d-flex align-items-center mb-4">
+			<h2 class="flex-shrink-0">Why Choose <span class="text-primary">Sundance</span>?</h2>
+			<hr class="flex-shrink-1 ms-4 w-100">
+		</div>
+		<div class="row gx-md-5 gy-5 align-items-center mb-5">
+			<div class="col-md-5">
+				<img src="why1.jpg" class="img-fluid rounded-4 shadow" alt="Hotshot truck with assorted materials loaded on the trailer">
+			</div>
+			<div class="col-md-7">
+				<p class="lead text-body-secondary">When you pick our family-owned trucking crew, you're not just choosing a service – you're joining our family. We're all about flexibility, making sure we adapt to what works best for you. And since we know Sacramento like the back of our hand, you can bet we'll find the quickest routes and dodge any local hiccups along the way. Count on us for a reliable ride for your stuff!</p>
+			</div>
+		</div>
+		<div class="row gx-md-5 gy-5 mx-auto" style="max-width:900px;">
+			<div class="col-md-4 text-center">
+				<div class="d-inline-block bg-primary bg-gradient p-4 rounded-4 shadow mb-3">
+					<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#ffffff" class="bi bi-box-seam-fill" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.01-.003.268-.108a.75.75 0 0 1 .558 0l.269.108.01.003zM10.404 2 4.25 4.461 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339L8 5.961 5.596 5l6.154-2.461z"/></svg>
+				</div>
+				<p>Effortlessly navigate job sites with our piggy-back forklifts, ensuring seamless delivery right where you need it.</p>
+			</div>
+			<div class="col-md-4 text-center">
+				<div class="d-inline-block bg-primary bg-gradient p-4 rounded-4 shadow mb-3">
+					<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#ffffff" class="bi bi-geo-alt-fill" viewBox="0 0 16 16"><path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/></svg>
+				</div>
+				<p>Rely on our experienced team's expert knowledge of Northern and Central California for smooth logistics every step of the way.</p>
+			</div>
+			<div class="col-md-4 text-center">
+				<div class="d-inline-block bg-primary bg-gradient p-4 rounded-4 shadow mb-3">
+					<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#ffffff" class="bi bi-clock-history" viewBox="0 0 16 16"><path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022zm2.004.45a7 7 0 0 0-.985-.299l.219-.976q.576.129 1.126.342zm1.37.71a7 7 0 0 0-.439-.27l.493-.87a8 8 0 0 1 .979.654l-.615.789a7 7 0 0 0-.418-.302zm1.834 1.79a7 7 0 0 0-.653-.796l.724-.69q.406.429.747.91zm.744 1.352a7 7 0 0 0-.214-.468l.893-.45a8 8 0 0 1 .45 1.088l-.95.313a7 7 0 0 0-.179-.483m.53 2.507a7 7 0 0 0-.1-1.025l.985-.17q.1.58.116 1.17zm-.131 1.538q.05-.254.081-.51l.993.123a8 8 0 0 1-.23 1.155l-.964-.267q.069-.247.12-.501m-.952 2.379q.276-.436.486-.908l.914.405q-.24.54-.555 1.038zm-.964 1.205q.183-.183.35-.378l.758.653a8 8 0 0 1-.401.432z"/><path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0z"/><path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5"/></svg>
+				</div>
+				<p>We can accommodate next day deliveries and adapt to dynamic construction schedule changes.</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="bg-light">
+	<div class="container-fluid p-0">
+		<div class="row g-0 align-items-center">
 			<div class="col-md-6">
-				<img src="why1.jpg" class="img-fluid rounded-4 shadow-sm" alt="Hotshot truck with assorted materials loaded on the trailer">
+				<div class="container mx-auto" style="max-width: 450px;">
+					<div class="d-flex align-items-center mb-4">
+						<h2 class="flex-shrink-0">Get in Touch!</h2>
+						<hr class="flex-shrink-1 ms-4 w-100">
+					</div>
+					<form action="https://formspree.io/sundancetransportinc@gmail.com" method="POST">
+						<input type="hidden" name="_subject" value="Sundance Contact Form">
+						<input type="hidden" name="_next" value="https://sundancetransportinc.com">
+						<div class="mb-3">
+							<label class="form-label fw-bold" for="name">Name</label>
+							<input class="form-control" type="text" name="name" placeholder="Name" required="">
+						</div>
+						<div class="mb-3">
+							<label class="form-label fw-bold" for="_replyto">Email</label>
+							<input class="form-control" type="email" name="_replyto" placeholder="Email" required="">
+						</div>
+						<div class="mb-3">
+							<label class="form-label fw-bold" for="message">Message</label>
+							<textarea class="form-control" name="message" rows="5" placeholder="Your message" required=""></textarea>
+						</div>
+						<div class="mb-3 text-center">
+							<input class="btn btn-primary px-4" type="submit" value="Submit Form">
+						</div>
+					</form>
+				</div>
 			</div>
 			<div class="col-md-6">
-				<h2>Why Choose <span class="text-primary">Sundance</span>?</h2>
-				<p>When you pick our family-owned trucking crew, you're not just choosing a service – you're joining our family. We're all about flexibility, making sure we adapt to what works best for you. And since we know Sacramento like the back of our hand, you can bet we'll find the quickest routes and dodge any local hiccups along the way. Count on us for a laid-back yet reliable ride for your stuff!</p>
+				<img src="contact1.jpg" class="w-100 h-100 object-fit-cover">
 			</div>
 		</div>
 	</div>
